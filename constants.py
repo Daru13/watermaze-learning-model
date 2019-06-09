41085c22b51635798d87aa0cc9956835ad71c3db
Daru13 <daru14@gmail.com>
2019-06-09 08:03:48 +0200

    Update some ML constants for testing purpose.

--- constants.py
+++ constants.py
@@ -1,22 +1,22 @@
 # Constants
 
-X_ORIGIN = 0.0                      # coordinate
-Y_ORIGIN = 0.0                      # coordinate
+X_ORIGIN = 0.0                          # coordinate
+Y_ORIGIN = 0.0                          # coordinate
 
-WATERMAZE_RADIUS = 1.0              # meters
-PLATEFORM_RADIUS = 0.05             # meters
+WATERMAZE_RADIUS = 1.0                  # meters
+PLATEFORM_RADIUS = 0.05                 # meters
 
-SWIMING_SPEED = 0.8                 # meters/second
-SWIMING_MOMENTUM_RATIO = 1/3        # % of momentum in the movement
+SWIMING_SPEED = 0.8                     # meters/second
+SWIMING_MOMENTUM_RATIO = 1/3            # % of momentum in the movement
 
-NB_PLACE_CELLS = 493                # quantity
-PLACE_CELL_STD = 0.16               # meters
+NB_PLACE_CELLS = 493                    # quantity
+PLACE_CELL_STD = 0.16                   # meters
 
-NB_ACTIONS = 8                      # quantity
+NB_ACTIONS = 8                          # quantity
 
-LEARNING_RATE = 0.6                 # RL parameter
-CRITIC_WEIGHTS_UPDATE_SCALE = 0.1  # RL parameter
-ACTOR_WEIGHTS_UPDATE_SCALE = 1.0    # RL parameter
+LEARNING_RATE = 0.8                     # RL parameter
+CRITIC_WEIGHTS_UPDATE_SCALE = 0.1       # RL parameter
+ACTOR_WEIGHTS_UPDATE_SCALE = 0.5        # RL parameter
 
-TIME_PER_STEP = 0.1                 # seconds/simulation step
-TRIAL_TIMEOUT = 120                 # seconds
+TIME_PER_STEP = 0.1                     # seconds/simulation step
+TRIAL_TIMEOUT = 120                     # seconds
