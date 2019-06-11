bab4ced807d668bcc78612aae372a92727430336
Daru13 <daru14@gmail.com>
2019-06-12 01:17:43 +0200

    Print "titles" to indicate which experiment is about to be simulated.

--- main.py
+++ main.py
@@ -5,19 +5,24 @@ from tqdm import tqdm
 
 from experiments import RMW, DMP
 from plot import plot_trial
+from utilities import print_title
 
 
 # Reference Memory in the Watermaze (RMW) experiment
+print_title("Reference Memory in the Watermaze (RMW)")
+
 rmw = RMW()
-logs_of_all_runs = rmw.run_n_times(30)
+logs_of_all_runs = rmw.run_n_times(50)
 
 rmw.plot_rat_performance(logs_of_all_runs)
 rmw.plot_one_run(logs_of_all_runs[-1])
 
 
 # Delayed Matching-to-Place (DMP) experiment
+print_title("Delayed Matching-to-Place (DMP)")
+
 dmp = DMP()
-logs_of_all_runs = dmp.run_n_times(30)
+logs_of_all_runs = dmp.run_n_times(50)
 
 dmp.plot_rat_performance(logs_of_all_runs)
 dmp.plot_one_run(logs_of_all_runs[-1])
